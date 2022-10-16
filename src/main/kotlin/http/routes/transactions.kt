8b99319374d68ec3b83e5a458aec568a7f7cd333
JacobGirling98 <jacob.girling98@gmail.com>
2022-10-16 22:48:45 +0100

    post multiple transactions routes

--- src/main/kotlin/http/routes/transactions.kt
+++ src/main/kotlin/http/routes/transactions.kt
@@ -3,10 +3,7 @@ package http.routes
 import dao.Database
 import domain.TransactionType.CREDIT
 import domain.TransactionType.DEBIT
-import http.handlers.postBankTransferHandler
-import http.handlers.postCreditDebitHandler
-import http.handlers.postIncomeHandler
-import http.handlers.postPersonalTransferHandler
+import http.handlers.*
 import org.http4k.core.Method.POST
 import org.http4k.routing.bind
 import org.http4k.routing.routes
@@ -17,6 +14,13 @@ fun transactionRoutes(database: Database) = routes(
         "/debit" bind POST to postCreditDebitHandler(DEBIT) { database.save(it) },
         "/bank-transfer" bind POST to postBankTransferHandler { database.save(it) },
         "/personal-transfer" bind POST to postPersonalTransferHandler { database.save(it) },
-        "/income" bind POST to postIncomeHandler { database.save(it) }
+        "/income" bind POST to postIncomeHandler { database.save(it) },
+        "/multiple" bind routes(
+            "/credit" bind POST to postCreditDebitListHandler(CREDIT) { database.save(it) },
+            "/debit" bind POST to postCreditDebitListHandler(DEBIT) { database.save(it) },
+            "/bank-transfer" bind POST to postBankTransferListHandler { database.save(it) },
+            "/personal-transfer" bind POST to postPersonalTransferListHandler { database.save(it) },
+            "/income" bind POST to postIncomeListHandler { database.save(it) },
+        )
     )
 )
