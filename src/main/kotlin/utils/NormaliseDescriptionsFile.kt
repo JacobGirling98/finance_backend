06c58cf4cf1a272f7ffeeef6c0c21513d5492dbb
JacobGirling98 <jacob.girling98@gmail.com>
2022-10-31 21:06:58 +0000

    change normalise descriptions script to use txt file not json file

--- src/main/kotlin/utils/NormaliseDescriptionsFile.kt
+++ src/main/kotlin/utils/NormaliseDescriptionsFile.kt
@@ -3,15 +3,16 @@ package utils
 import config.CustomJackson
 import config.DATA_LOC
 import dao.ReferenceData
+import domain.DescriptionMapping
 import java.io.File
 
-private val referenceData = ReferenceData(DATA_LOC)
+
 
 fun main() {
-    referenceData.initialise()
-    File("$DATA_LOC/description_mappings.json").delete()
+    val json = CustomJackson.mapper.readValue(File("$DATA_LOC/description_mappings.json").readText(), Array<DescriptionMapping>::class.java).toList()
+//    File("$DATA_LOC/description_mappings.json").delete()
     val file = File("$DATA_LOC/description_mappings.txt")
-    referenceData.descriptions.forEach {
+    json.forEach {
         file.appendText("${CustomJackson.mapper.writeValueAsString(it)}\n")
     }
 }
