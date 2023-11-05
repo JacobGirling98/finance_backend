b88ba7dd2a3e454e0e7f46175f5c3b6630473c51
JacobGirling98 <jacob.girling98@gmail.com>
2023-11-05 20:56:18 +0000

    new deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-./start-docker.sh
-
-container_name=finance-backend
-
-docker build -t $container_name ../.
-docker save finance-backend | gzip -c > backend.tar.gz
-
-scp backend.tar.gz pi:~/Programming/finance/backend.tar.gz
-
-rm backend.tar.gz
-docker image rm $container_name
-
-finance_dir=/home/jacobg/Programming/finance
-
-ssh pi "cd $finance_dir && docker load < backend.tar.gz"
-
-./install-on-pi.sh
+./build/run.sh
+./teardown/run.sh
+./run/run.sh
---
+++ deploy/run/run-app-pi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -e PROFILE=docker -v /home/jacobg/Programming/finance/finance_data:/app/data -v /home/jacobg/Programming/finance/google-credentials:/app/config -p 9000:9000 -d --name finance-backend -t finance-backend
