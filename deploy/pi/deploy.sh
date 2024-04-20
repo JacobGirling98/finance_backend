938f83bc46e8a96d8836408900642f5701e10526
JacobGirling98 <jacob.girling98@gmail.com>
2024-04-20 17:43:29 +0100

    deploy.sh

--- deploy/pi/deploy.sh
+++ deploy/pi/deploy.sh
@@ -4,7 +4,7 @@ start_app() {
   container_id=$(docker ps -q -f name=$container_name)
 
   if [ -n "$container_id" ]; then
-    echo "Finance Backend: app is already running, restarting now."
+    echo "$log_prefix app is already running, restarting now."
     docker container stop $container_name > /dev/null
   fi
 
@@ -12,23 +12,24 @@ start_app() {
 
   container_id=`docker run -e PROFILE=docker -v /home/jacobg/Programming/finance/finance_data:/app/data -v /home/jacobg/Programming/finance/google-credentials:/app/config -p 9000:9000 -d --name finance-backend -t finance-backend`
 
-  echo "Finance Backend: started with container id $container_id"
+  echo "$log_prefix started with container id $container_id"
 }
+
+log_prefix="Finance Backend:"
+container_name=finance-backend
 
 git_pull_output=$(git pull 2>&1)
 
-echo $git_pull_output
-
 if [ $? -eq 0 ] || [ $git_pull_output =~ "Already up-to-date" ]; then
-  echo "Finance Backend: up to date, skipping Docker build..."
+  echo "$log_prefix up to date, skipping Docker build..."
 
   start_app
 
   exit 0
 fi
 
-echo "Finance Backend: there are unbuilt changes, starting build now."
+echo "$log_prefix there are unbuilt changes, starting build now."
 
-docker build -t finance-backend ../.
+docker build -t $container_name ../.
 
 start_app
