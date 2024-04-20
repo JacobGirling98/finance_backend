b59e32e3de3907b8b7999ab67917ed75913483b8
JacobGirling98 <jacob.girling98@gmail.com>
2024-04-20 17:48:00 +0100

    testing new git logic

--- deploy/pi/deploy.sh
+++ deploy/pi/deploy.sh
@@ -24,11 +24,15 @@ git fetch
 # Store the current HEAD commit hash
 OLD_HEAD=$(git rev-parse HEAD)
 
+echo $OLD_HEAD
+
 # Merge the fetched commits
 git merge
 
 # Store the new HEAD commit hash
 NEW_HEAD=$(git rev-parse HEAD)
+
+echo $NEW_HEAD
 
 # Compare OLD_HEAD and NEW_HEAD
 if [ "$OLD_HEAD" = "$NEW_HEAD" ]; then
