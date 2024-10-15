f31b9e8b69eb10053b99f7e59f51fa42727b98b8
Jacob <jacob.girling98@gmail.com>
2024-10-15 12:50:07 +0100

    build fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
 
 jib {
     from {
-        image = "openjdk:17-jdk-slim"
+        image = "linux/arm64@openjdk:17-jdk-slim"
     }
     to {
         image = "finance-backend"
